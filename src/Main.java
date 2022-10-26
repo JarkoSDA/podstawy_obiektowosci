d5735100bb93040213320fd723b9b504174ec8bb
Jarek <jarek.malesza@gmail.com>
2022-10-26 19:59:07 +0200

    zmiana imienia krolika

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Rabbit krolik1 = new Rabbit( 50, "Henek", true );
+        krolik1.changeName("Stanislaw");
     }
 }
---
+++ src/Rabbit.java
@@ -0,0 +1,15 @@
+public class Rabbit {
+    private int lenght;
+    private String name;
+    private boolean softness;
+
+    Rabbit(int lenght, String name, boolean softness ){
+        this.lenght = lenght;
+        this.name = name;
+        this.softness = softness;
+    }
+    public void changeName(String name){
+        this.name = name;
+    }
+
+}
